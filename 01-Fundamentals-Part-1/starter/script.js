c03b163615945ad629a82080d52b3f902da8ec45
Abhinav <AbhinavSunil@hotmail.com>
2025-10-06 16:38:08 +0530

    fundamentals par1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+// console.log('hi iam Abhinav');
+
+// let fname = 'Abhinav';
+// let isgender = true;
+// let age;
+// console.log(fname);
+// console.log(isgender);
+// console.log(age);
+// fname = 'Sunil';
+// console.log(fname);
+// let age = 30;
+// console.log(age);
+// age = 25;
+// console.log(age);
+// const fname = 'Abhinav';
+// console.log(fname);
+// fname = 'Sunil';
