3a93bb1202e7e3376f6a1132845724a747822e8b
Abhinav <AbhinavSunil@hotmail.com>
2025-10-11 23:52:33 +0530

    add guess my number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.score').textContent = 100;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.guess').value = 23;
+
+let score = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = score;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === score) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > score) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+  } else if (guess < score) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+  }
+});
